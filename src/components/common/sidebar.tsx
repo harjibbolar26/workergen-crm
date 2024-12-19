f19a8109d4cefcb97e695588379c54d354f5e043
Jibola Paul <harjibbolar26@gmail.com>
2024-12-19 09:53:54 +0100

    first commit

---
+++ src/components/common/sidebar.tsx
@@ -0,0 +1,80 @@
+import * as React from "react";
+import { ChevronDown } from "lucide-react";
+
+import { NavItem, navItems } from "@/config/sidebar";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarContent>
+        {navItems.map((item) => (
+          <SidebarGroup key={item.title}>
+            {item.items?.length ? (
+              <Collapsible defaultOpen className="group/collapsible">
+                <SidebarGroupLabel asChild>
+                  <CollapsibleTrigger className="flex w-full items-center justify-between">
+                    {item.title}
+                    <ChevronDown className="size-4 shrink-0 transition-transform duration-200 group-data-[state=open]/collapsible:rotate-180" />
+                  </CollapsibleTrigger>
+                </SidebarGroupLabel>
+                <CollapsibleContent>
+                  <SidebarGroupContent>
+                    <SidebarMenu>
+                      {item.items?.map((subItem) => (
+                        <SidebarMenuItem key={subItem.title}>
+                          <SidebarMenuButton asChild>
+                            <a href="#">
+                              {subItem.icon && (
+                                <subItem.icon className="size-4" />
+                              )}
+                              <span>{subItem.title}</span>
+                            </a>
+                          </SidebarMenuButton>
+                        </SidebarMenuItem>
+                      ))}
+                    </SidebarMenu>
+                  </SidebarGroupContent>
+                </CollapsibleContent>
+              </Collapsible>
+            ) : (
+              <>
+                <SidebarGroupContent>
+                  <SidebarMenu>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton asChild>
+                        <a href="#">
+                          {item.icon && <item.icon className="size-4" />}
+                          <span>{item.title}</span>
+                          {item.items && (
+                            <ChevronDown className="ml-auto size-4" />
+                          )}
+                        </a>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  </SidebarMenu>
+                </SidebarGroupContent>
+              </>
+            )}
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
---
+++ src/config/sidebar.ts
@@ -0,0 +1,131 @@
+import {
+  BarChart3,
+  BookOpen,
+  Building2,
+  Contact2,
+  FileSpreadsheet,
+  FolderOpen,
+  GanttChartSquare,
+  Home,
+  ListTodo,
+  Mail,
+  Package,
+  PieChart,
+  Quote,
+  Receipt,
+  ScrollText,
+  ShoppingCart,
+  Users2,
+} from "lucide-react";
+
+export interface NavItem {
+  title: string;
+  icon?: any;
+  items?: NavItem[];
+}
+
+export const navItems: NavItem[] = [
+  {
+    title: "Home",
+    icon: Home,
+  },
+  {
+    title: "Recent",
+    icon: FolderOpen,
+    items: [],
+  },
+  {
+    title: "Pinned",
+    icon: ListTodo,
+  },
+  {
+    title: "My work",
+    items: [
+      {
+        title: "Sales accelerator",
+        icon: BarChart3,
+      },
+      {
+        title: "Dashboards",
+        icon: PieChart,
+      },
+      {
+        title: "Activities",
+        icon: GanttChartSquare,
+      },
+    ],
+  },
+  {
+    title: "Customers",
+    items: [
+      {
+        title: "Accounts",
+        icon: Building2,
+      },
+      {
+        title: "Contacts",
+        icon: Contact2,
+      },
+    ],
+  },
+  {
+    title: "Sales",
+    items: [
+      {
+        title: "Leads",
+        icon: Users2,
+      },
+      {
+        title: "Opportunities",
+        icon: FileSpreadsheet,
+      },
+      {
+        title: "Competitors",
+        icon: Users2,
+      },
+    ],
+  },
+  {
+    title: "Collateral",
+    items: [
+      {
+        title: "Quotes",
+        icon: Quote,
+      },
+      {
+        title: "Orders",
+        icon: ShoppingCart,
+      },
+      {
+        title: "Invoices",
+        icon: Receipt,
+      },
+      {
+        title: "Products",
+        icon: Package,
+      },
+      {
+        title: "Sales Literature",
+        icon: BookOpen,
+      },
+    ],
+  },
+  {
+    title: "Marketing",
+    items: [
+      {
+        title: "Marketing lists",
+        icon: ScrollText,
+      },
+      {
+        title: "Quick Campaigns",
+        icon: Mail,
+      },
+    ],
+  },
+  {
+    title: "Performance",
+    icon: BarChart3,
+    items: [],
+  },
+];
